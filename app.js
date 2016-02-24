f8070db3badaa7f868b0a6ac2ebefdef7748ed74
Andrew London <London@Londons-Computer.local>
2016-02-23 22:37:52 -0500

    Tabs and checkboxes working

--- app.js
+++ app.js
@@ -1,10 +1,59 @@
-function addListItem () {
-  var text = $('#todoInput').val();
-  $('.todoAdded').append('<div class="todoItem">' + text + '</div>');
-  $('#todoInput').val('');
+var todos = []
+
+
+
+
+
+var templates = {
+  todoItem: [
+    '<div class="todoItem">',
+    '<%= todoItem %>',
+    '</div>'
+  ].join('')
 }
 
 
+function addTodo(newTodo) {
+  todos.push(newTodo)
+}
+
+function getToDoFromDom () {
+  var content = $('input[name="todoInput"]').val();
+  return {
+    content: content,
+  };
+}
+
+function addTodoItem (el) {
+  el.preventDefault();
+  var text = $('#todoInput').val();
+  $('.todoAdded').append("<div><input type='checkbox' class='done'/>" + text + "</div>");
+  $('#todoInput').val('');
+  var newTodo = getToDoFromDom();
+  addTodo(newTodo);
+  console.log(todos)
+};
+
+function finishItem () {
+  if ($(this).parent().css('textDecoration') == 'line-through') {
+    $(this).parent().css('textDecoration', 'none')
+  }
+  else {
+  $(this).parent().css('textDecoration', 'line-through');
+  $(this).parent().css('fontColor', 'lightgray')
+
+}
+};
+
 $(function() {
-  $('#todoInput').on("change", addListItem);
-})
+  $('#todoInput').on("change", addTodoItem);
+  $('body').on("click", ".done", finishItem);
+});
+
+$(".tabs").on("click", function(event) {
+  event.preventDefault();
+  $("section").removeClass("show");
+  $(this).addClass("show");
+  var selector = "." + $(this).attr("rel");
+  $(selector).addClass("show");
+});
