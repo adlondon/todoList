c5430e1bf6df1bb7d5e299bf3395c6b854ad78e3
Andrew London <london.andrewd@gmail.com>
2016-04-17 23:25:40 -0400

    uploaded wireframe for readme

--- test.js
+++ test.js
@@ -1,170 +1,147 @@
-$(document).ready(function() {
-  addAllTodos(todos);
-})
-
-
-var todos = [
-  {
-    content: 'I AM TEST',
-    completed: false,
+var page = {
+  init: function () {
+    page.initEvents();
+    page.initStyling();
   },
-  {
-    content: 'THIS IS FANTASTIC',
-    completed: false
-  }
-]
+// CLICK AND HOVER EVENTS
+  initEvents: function () {
 
-
-
-
-
-// CANT FIGURE OUT HOW TO INCLUDE TEMPLATES
-// var templates = {
-//   todoItem: [
-//     '<div class="todoItem" data-idx ="<%= idx %>">',
-//     '<%= todoItem %>',
-//     '</div>'
-//   ].join('')
-// }
-
-
-function addTodo(newTodo) {
-  todos.push(newTodo)
-}
-
-function getTodos() {
-  return todos;
-}
-
-function addTodoToDom(todoData, templateStr, $target) {
-  var tmpl = _.template(templateStr);
-  $target.append(tmpl(todoData));
-}
-
-function addAllTodos(arr) {
-  $('.all').html('');
-  _.each(arr, function(el,index) {
-    el.idx = index;
-    addTodoToDom(el,templates.todoPost, $(".all"))
-  });
-}
-
-function getToDoFromDom(text) {
-  return {
-    content: text,
-    completed: false,
-  };
-}
-
-
-$('form').on("submit", function(el) {
-  el.preventDefault();
-
-  var textOfInput = $('#todoInput').val();
-  var newTodo = getToDoFromDom(textOfInput);
-  // $('.todoAdded').append("<div class='active' data-todoItem ='<%= idx %>'><input type='checkbox' class='done'/>" + text + "</div>");
-  addTodo(newTodo);
-  $('#todoInput').val('');
-  addAllTodos();
-});
-
-
-
-// $('form').on("submit", function(el) {
-//   el.preventDefault();
-//
-//   var textOfInput = $('#todoInput').val();
-//   var newTodo = getToDoFromDom(textOfInput);
-//   // $('.todoAdded').append("<div class='active' data-todoItem ='<%= idx %>'><input type='checkbox' class='done'/>" + text + "</div>");
-//   addTodo(newTodo);
-//   $('#todoInput').val('');
-//   addAllTodos();
-// });
-
- // $('.todoAdded').append("<div class='active'><input type='checkbox' class='done'/>" + text + "</div>");
- // var newTodo = getToDoFromDom();
- // addTodo(newTodo);
- // console.log(todos)
- // $('#todoInput').val('');
-
-// *******************TABS
-
-// function getFilter(arr) {
-//   var result = [];
-//   if ($('.show').text() === "Active") {
-//     result = arr.filter(function(el, idx, arr){
-//       arr[idx].idx = idx;
-//       return el.completed === false
-//     });
-//   } else if ($('.todoMenuActive').text() === "Completed") {
-//     result = arr.filter(function(el, idx, arr){
-//       arr[idx].idx = idx;
-//       return el.completed === true
-//     })
-//   } else {
-//     result = arr;
-//   }
-  // return result;
-  function todoFilter() {
-todos.filter(function(el) {
-  return el.completed === false
-});
-}
-function toggleComplete(idx) {
-  getTodos()[idx].completed = !getTodos()[idx].completed;
-}
-  $('body').on("click", ".done", function (event) {
-    var idx = parseInt($(this).closest('div').data('todoitem'));
-    toggleComplete(idx);
-    console.log("index", idx);
-    if (!getTodos()[idx].completed) {
-
+    $('form').on("submit", onEnter);
+    $('body').on("click", ".done", checkClick);
+    $(".tabs").on("click", tabClick);
+  },
+  onEnter: function(el) {
+    el.preventDefault();
+    var textOfInput = $('#todoInput').val();
+    var newTodo = getToDoFromDom(textOfInput);
+    addTodo(newTodo);
+    $('#todoInput').val('');
+    addAllTodos(todos,$('.all'));
+  },
+  checkClick: function (el,idx) {
+    if ($(this).parent().hasClass("completed")) {
       $(this).parent().removeClass("completed")
       $(this).parent().addClass("active")
-      // $(this).parent().css('textDecoration', 'none')
-      // $(this).parent().css('color', 'black')
-
-
-      console.log(todos[idx].completed);
-
+      $(this).parent().css('textDecoration', 'initial')
+      $(this).parent().css('color', 'black')
+      toggleComplete($(this).parent().data("todoitem"))
+      counter()
     }
-    else {
+    else if($(this).parent().hasClass("active")) {
     $(this).parent().removeClass("active")
     $(this).parent().addClass("completed")
-    // $(this).parent().css('textDecoration', 'line-through');
-    // $(this).parent().css('color', 'lightgray')
-    console.log("todos should be here", todos[idx].completed);
-
+    $(this).parent().css('textDecoration', 'line-through');
+    $(this).parent().css('color', 'lightgray')
+    toggleComplete($(this).parent().data("todoitem"))
+    counter()
     }
-});
-var result = [];
-$(".tabs").on("click", function(event) {
-  event.preventDefault();
-  // $("section").removeClass("show");
-  // $(this).addClass("show");
-  // var selector = "." + $(this).attr("rel");
-  // $(selector).addClass("show");
-  if ($(this).text() === "Active") {
-    console.log("ACTIVE CLICKED", $(this))
-    result = todos.filter(function(el,idx) {
-      todos[idx].idx = idx; // ********Brandon helped me with this
-      return el.completed === false
-    });
-    console.log(result);
-
-  }
-  else if($(this).text() === "Completed") {
-    console.log("completed CLICKED", $(this))
+},
+tabClick: function(event) {
+  event.preventDefault()
+  console.log($(this).text());
+  if ($(this).text() === "Completed") {
     result = todos.filter(function(el,idx) {
       todos[idx].idx = idx;
       return el.completed === true
-    })
+    });
     console.log(result);
-    }
-  else if($(this).text() === "All"){
-    console.log("all CLICKED", $(this))
-    result = todos
+    addAllTodos(result,$('.completed'));
   }
-  addAllTodos(result);
+  else if ($(this).text() === "Active") {
+    result = todos.filter(function(el,idx) {
+      todos[idx].idx = idx;
+      return el.completed === false
+    });
+    addAllTodos(result,$('.active'));
+  }
+  else {
+    addAllTodos(todos,$('.all'))
+  }
+  $("section").removeClass("show");
+  var selector = "." + $(this).attr("rel");
+  $(selector).addClass("show");
+},
+// *********** PAGELOAD EVENTS
+  initStyling: function () {
 
+// this is important
+  addAllTodos(todos,$('.all'));
+
+
+    function addAllTodos(arr,$target) {
+      $target.html("");
+      console.log("ADD ALL TODOS FIRE")
+      _.each(arr, function(el,index) {
+        el.idx = index;
+        addTodoToDom(el,templates.todoPost, $target);
+      });
+      counter()
+    }
+
+    function toggleComplete(idx) {
+
+      todos[idx].completed = !todos[idx].completed;
+    }
+
+
+
+
+  function deleteTodo(idx) {
+    todos.splice(idx, 1);
+  }
+  $('section').on('click', '.delete', function (event) {
+    var idx = $(this).parent().data('todoitem');
+    console.log(idx)
+    $(this).parent().remove();
+    deleteTodo(idx);
+  });
+  function addTodo(newTodo) {
+    todos.push(newTodo)
+  }
+
+  function getTodos() {
+    return todos;
+  }
+
+  function addTodoToDom(todoData, templateStr, $target) {
+    var tmpl = _.template(templateStr);
+    $target.append(tmpl(todoData));
+  }
+  function getToDoFromDom(text) {
+    return {
+      content: text,
+      completed: false,
+    };
+  }
+},
+};
+
+
+
+
+
+
+
+$(document).ready(function() {
+
+  page.init();
 });
+
+var todos = [];
+var result = [];
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// *******************TABS
